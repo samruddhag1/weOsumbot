1b1c739c1b78436c719cd2dc056b0d3f8c0663ff
harsgak <harsgak@yahoo.com>
2016-12-07 06:05:59 +0530

    Implemented /iOsum conversation
    * Implemented the discussed state diagram (See outline image)
    * Seperated database into a new file.
    Note : The new code is in commans2.py file. The old code will be deleted
    soon (When the code structure is more stable).

--- commands.py
+++ commands.py
@@ -6,22 +6,32 @@ import telegram
 from telegram.ext import Updater
 
 #States 
-VERIFYAMOUNT,VERIFYPERSON = range(2)
+VERIFYAMOUNT,VERIFYPERSON = 0,1
 END = -1
 
+#############################################################
+#   iosum conversation functions
+#   
+#
 
 def askAmount(bot , update):
+    """
+    - Asks user for amount.
+    - Changes state to VERIFYAMOUNT when done 
+    """
+    
     text = "How much?"
     bot.sendMessage(chat_id=update.message.chat_id, text=text)
+    
     print ('asked amount')
     
     return VERIFYAMOUNT
-def askPerson(bot, update):
-    text = "Who's e (he/she/whatever)?"
-    bot.sendMessage(chat_id=update.message.chat_id, text=text)
-    print ('asked person')
-    
+
 def verifyAmount(bot, update):
+    """
+    - Verifies if amount is valid.
+    - 
+    """
     str_amt=update.message.text
     print('verify_caught')
     
@@ -33,17 +43,31 @@ def verifyAmount(bot, update):
         
         askPerson(bot, update)
         return VERIFYPERSON
-        
     except:
         try_again = "Sorry, could not read a *valid* amount!!\nWhats valid? Try something you would type in a calci.\n"
         try_again+= "You can also type _/cancel_ to abort"
         
         bot.sendMessage(chat_id=update.message.chat_id, text = try_again, parse_mode=telegram.ParseMode.MARKDOWN)
         return VERIFYAMOUNT
-    pass
+    
+def askPerson(bot, update):
+    
+    text = "Who is the other person?"
+    bot.sendMessage(chat_id=update.message.chat_id, text=text)
+    print ('asked person')
+    
+    return VERIFYPERSON
+    
+
+        
+
 
 #def verifyPerson(bot, update):
     
+
+#
+#
+##############################################################
 
 def cancel(bot, update):
     text = 'Ok. Going back to my world.'
@@ -59,6 +83,8 @@ def askContact(bot,update):
      reply_markup = telegram.ReplyKeyboardMarkup(custom_keyboard)
      bot.sendMessage(chat_id=update.message.chat_id, text="Would mind to share location and contact with me ?",reply_markup=reply_markup) 
         
+        
+        
 ######Temp for Testing Only######
 updater = Updater(token='292385752:AAFCJrNmm_x47FFk_VzJqQFToDnZ7BdQKCU')
 dispatcher = updater.dispatcher
@@ -69,7 +95,8 @@ from telegram.ext import MessageHandler , Filters
 cancel_handler = CommandHandler('cancel',cancel)
 iosum_handler = CommandHandler('iosum',askAmount)
 verifyAmount_handler = MessageHandler([Filters.text ], verifyAmount)
-iosumconv_handler = ConversationHandler([iosum_handler],{VERIFYAMOUNT: [verifyAmount_handler]},[cancel_handler] )
+
+iosumconv_handler = ConversationHandler([iosum_handler],{VERIFYAMOUNT: [verifyAmount_handler] },[cancel_handler] )
 echo_handler = MessageHandler([Filters.text], echo)
 contact_handler = CommandHandler('contact',askContact)
 #.add_handler(iosum_handler)
---
+++ commands2.py
@@ -0,0 +1,218 @@
+# -*- coding: utf-8 -*-
+"""
+This file is for  functions that are directly tied to the bot commands.
+"""
+
+from telegram import (ReplyKeyboardMarkup, ReplyKeyboardHide)
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters, RegexHandler,
+                          ConversationHandler, CallbackQueryHandler)
+
+import logging
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+CHOOSING, TYPING_REPLY, TYPING_CHOICE = range(3)
+
+PROCESSAMOUNT, CONFIRMAMOUNT, PROCESSREASON, CONFIRMREASON, GENERATETOKEN = range(5)
+
+reply_keyboard = [['7', '8', '9'],
+                  ['4', '5', '6'],
+                  ['1', '2', '3'],
+                  ['.', '0', '+'],
+                  ['*', '/', '-']]
+calc_markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
+
+
+def iOsum(bot , update):
+    """
+    Osum converstion starts.
+    """
+    update.message.reply_text(
+        'Hi! Looks like you owe somebody. '
+        'I will help you remember your dues'
+        'Send /cancel to stop talking to me.\n\n')
+    
+    logger.info('/iOsum by user: {}'.format(update.message.from_user))    
+    return askAmount(bot, update)
+
+
+def askAmount(bot , update):
+    """
+    - Asks user for amount.
+    - Changes state to PROCESSAMOUNT when done 
+    """
+    update.message.reply_text(
+        'How much exactly do you owe?',
+        reply_markup=calc_markup)
+        
+    return PROCESSAMOUNT
+
+
+def verify_amount(str_amt):
+    """
+    - Verifies if amount is valid.
+    """
+    logger.info('Verifing: {}'.format(str_amt))
+    try :
+        amount = eval(str_amt)
+        logger.info('Valid Amount: {}'.format(str_amt))
+    except :
+        amount = False
+        logger.info('Invalid Amount: {}'.format(str_amt))
+    
+    return amount
+
+
+def process_amount(bot, update):
+    """
+    - Verifies if amount is valid.
+    - If not redo
+    - Adds it to userdata.
+    - Changes state to PROCESSREASON 
+    """
+    user = update.message.from_user
+    amount = verify_amount(update.message.text)
+    print('Here!')
+    if amount:
+        logger.info("User:{} Amount: {}".format(user, amount))
+        
+        return confirmAmount(bot, update, amount)
+    else:
+        update.message.reply_text('Sorry! I could not understand that.')
+        logger.info("Asking {} amount again".format(user) )  
+        
+        return askAmount(bot, update)
+
+
+def confirmAmount(bot, update, amount):
+    """
+    - Asks user to confirm amount.
+    """
+    update.message.reply_text(
+        'You owe {}\n Confirm?'.format(amount),
+         reply_markup=ReplyKeyboardMarkup([['Yes','No']], one_time_keyboard=True))
+    
+    return CONFIRMAMOUNT
+    
+def confirmerAmount(bot, update):
+    """
+    
+    """
+    response = update.message.text
+    if response == 'No':
+        return askAmount(bot, update)
+        
+    elif response == 'Yes':
+        return askReason(bot, update)
+
+def askReason(bot, update):
+    """
+    - Asks for O reason
+    """
+    update.message.reply_text(
+        'Any reason for this owe?'
+        'Type reason or press skip to skip',
+        reply_markup=ReplyKeyboardMarkup([['skip']], one_time_keyboard=True))
+    
+    return PROCESSREASON
+    
+def process_reason(bot, update):
+    """
+    
+    """
+    reason = update.message.text
+    if reason == 'skip':
+        reason = ''
+    return confirmReason(bot, update, reason)
+
+def confirmReason(bot, update, reason):
+    """
+    -Asks user to confirm reason
+    """
+    update.message.reply_text(
+        'You owe because: {}\n Confirm?'.format(reason),
+         reply_markup=ReplyKeyboardMarkup([['Yes','No']], one_time_keyboard=True))
+         
+    return CONFIRMREASON
+
+
+def confirmerReason(bot, update):
+    """
+    
+    """
+    response = update.message.text
+    if response == 'No':
+        return askReason(bot, update)
+        
+    elif response == 'Yes':
+        return token(bot, update)
+        
+        
+        
+def token(bot, update):
+    """
+    """
+    update.message.reply_text('Bye! We are Done.',
+                              reply_markup=ReplyKeyboardHide())    
+    return ConversationHandler.END
+    
+def cancel(bot, update):
+    user = update.message.from_user
+    logger.info("User {} canceled the conversation.".format(user))
+    update.message.reply_text('Bye! I hope we can talk again some day.',
+                              reply_markup=ReplyKeyboardHide())
+
+    return ConversationHandler.END
+
+def error(bot, update, error):
+    logger.warn('Update "%s" caused error "%s"' % (update, error))
+
+
+def main():
+    # Create the EventHandler and pass it your bot's token.
+    updater = Updater("215450926:AAELiS1y9NIczfgy19KO48mnlMVrcf4xVCs")
+
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+
+    # Add conversation handler with the states GENDER, PHOTO, LOCATION and BIO
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('iOsum', iOsum)],
+
+        states={
+            PROCESSAMOUNT: [MessageHandler(Filters.text, process_amount)],
+            
+            CONFIRMAMOUNT: [RegexHandler('^(Yes|No)$', confirmerAmount)],
+            
+            PROCESSREASON: [MessageHandler(Filters.text, process_reason)],
+            
+            CONFIRMREASON: [RegexHandler('^(Yes|No)$', confirmerReason)],
+            
+            GENERATETOKEN: [MessageHandler(Filters.text, token)]
+        },
+
+        fallbacks=[CommandHandler('cancel', cancel)]
+    )
+
+    dp.add_handler(conv_handler)
+
+    # log all errors
+    dp.add_error_handler(error)
+
+    # Start the Bot
+    updater.start_polling()
+    print('Started Bot')
+
+    # Run the bot until the you presses Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
